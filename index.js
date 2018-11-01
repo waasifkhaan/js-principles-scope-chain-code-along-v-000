c481f1c5ce126d96acddf1934f7f03d04eb31e68
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 23:41:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ var customerName = 'bob';
 const leastFavoriteCustomer = 'lala';
 
 
-function upperCaseCustomerName() {
-  return customerName = customerName.toUpperCase();
-}
-
-function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer = 'la';
-}
-
-
-function setBestCustomer(){
-  bestCustomer = 'not bob';
-}
-
-function overWriteBestCustomer(newFavorite){
-  bestCustomer = newFavorite;
-}
+// function upperCaseCustomerName() {
+//   return customerName = customerName.toUpperCase();
+// }
+// 
+// function changeLeastFavoriteCustomer(){
+//   leastFavoriteCustomer = 'la';
+// }
+// 
+// 
+// function setBestCustomer(){
+//   bestCustomer = 'not bob';
+// }
+// 
+// function overWriteBestCustomer(newFavorite){
+//   bestCustomer = newFavorite;
+// }
 function attemptTwoFavoriteCustomers() {
   let favoriteCustomer = 'll';
   let favoriteCustomer = 'llll';
