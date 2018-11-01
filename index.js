4ec5e94efb5f78474759eac208242dc9ec528a39
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 23:45:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 /* Your code goes here */
 var customerName = 'bob';
 const leastFavoriteCustomer = 'lala';
-// function upperCaseCustomerName() {
-//   return customerName = customerName.toUpperCase();
-// }
-//
-// function changeLeastFavoriteCustomer(){
-//   leastFavoriteCustomer = 'la';
-// }
-//
-//
-// function setBestCustomer(){
-//   bestCustomer = 'not bob';
-// }
-//
-// function overWriteBestCustomer(newFavorite){
-//   bestCustomer = newFavorite;
-// }
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'la';
+}
+
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(newFavorite){
+  bestCustomer = newFavorite;
+}
 function attemptTwoFavoriteCustomers() {
   let favoriteCustomer = 'll';
   let favoriteCustomer = 'llll';
