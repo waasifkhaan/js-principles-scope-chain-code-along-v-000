4f5433aca7a7ae921a2b30be4bc4056f944baf9d
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 23:42:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 /* Your code goes here */
 var customerName = 'bob';
 const leastFavoriteCustomer = 'lala';
-
-
 // function upperCaseCustomerName() {
 //   return customerName = customerName.toUpperCase();
 // }
-// 
+//
 // function changeLeastFavoriteCustomer(){
 //   leastFavoriteCustomer = 'la';
 // }
-// 
-// 
+//
+//
 // function setBestCustomer(){
 //   bestCustomer = 'not bob';
 // }
-// 
+//
 // function overWriteBestCustomer(newFavorite){
 //   bestCustomer = newFavorite;
 // }
